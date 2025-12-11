Testing ADP calculation, not working - looking for the bug

felixrefine.py
@@ -186,6 +186,11 @@ v.basis_atom_delta = np.zeros([n_basis, 3])  # ***********what's this
 inp_dict = px.read_inp_file('felix.inp')
 v.update_from_dict(inp_dict)
 
+if v.debug:
+    np.set_printoptions(precision=5, suppress=True)
+    for i in range(n_basis):
+        print(f"{v.basis_atom_label[i]}:  u_ij =\n {v.basis_u_ij[i, :, :]}")
+
 # thickness array
 if (v.final_thickness > v.initial_thickness + v.delta_thickness):
     v.thickness = np.arange(v.initial_thickness, v.final_thickness,
@@ -208,13 +213,14 @@ v.g_limit = v.g_limit * 2 * np.pi
 v.basis_pv = np.zeros(n_basis, dtype=float)
 v.basis_kappa = np.zeros(n_basis, dtype=float)
 atomic_number = np.array([fu.atomic_number_map[na] for na in v.basis_atom_name])
-print(type(v.basis_kappa))
-for i in range(n_basis):
-    v.basis_pv[i]= fu.elements_info[atomic_number[i]]["pv"]
-    v.basis_kappa[i] = 1.0  #set all kappa values to 1 initially 
 
-#setting up initial pv values 
-print(v.basis_kappa)
+for i in range(n_basis):
+    v.basis_pv[i] = fu.elements_info[atomic_number[i]]["pv"]
+    v.basis_kappa[i] = 1.0  # set all kappa values to 1 initially
+
+# setting up initial pv values
+if v.debug:
+    print(v.basis_kappa)
 
 '''
 v.Basis_Pv[0]= 0.9994
@@ -225,25 +231,15 @@ v.Basis_Kappa[1]= 1.01
 v.Basis_Kappa[2]= 1.01
 
 '''
-
-
 # kappas (default 1.0)
-
-
-#refined kappa : [1.21517673 1.12267508 0.93547286]
+# refined kappa : [1.21517673 1.12267508 0.93547286]
 # expand per atom in full unit cell
- 
+# print(unique_aniso_matrixes)
+# print(unique_aniso_matrixes.shape)
 
-
- #print(unique_aniso_matrixes)
- #print(unique_aniso_matrixes.shape)
- 
- # Step 1: define a dictionary of initial P_v guesses per element
- # For LiNbO3 using formal charges as we discussed
- # we just need a dictionary of the valence states of the atoms 
-
-
- 
+# Step 1: define a dictionary of initial P_v guesses per element
+# For LiNbO3 using formal charges as we discussed
+# we just need a dictionary of the valence states of the atoms
 
 # output
 print(f"Zone axis: {v.incident_beam_direction.astype(int)}")
@@ -251,8 +247,6 @@ if v.n_thickness == 1:
     print(f"Specimen thickness {v.initial_thickness/10} nm")
 else:
     print(f"{v.n_thickness} thicknesses: {', '.join(map(str, v.thickness/10))} nm")
-    
-    
 
 if v.scatter_factor_method == 0:
     print("Using Kirkland scattering factors")
@@ -335,6 +329,7 @@ v.atom_refine_vec = ([])  # the direction of atom movement, [0,0,0] if none
 nullvec = np.array([0, 0, 0])  # null vector for above
 if 'S' not in v.refine_mode:
     v.n_variables = 0
+    # v.iter_count = 1  # or should this be elsewhere ***
     # count refinement variables
     if 'B' in v.refine_mode:  # Atom coordinate refinement
         # the input v.atomic_sites gives the index of the atom in the cif
@@ -429,30 +424,20 @@ if 'S' not in v.refine_mode:
         v.refined_variable_type.append(9)
         v.atom_refine_flag.append(-1)
         v.atom_refine_vec.append(nullvec)  # no atom movement
-        
-        
-    
-    if  'J' in v.refine_mode:
-        
-        
+
+    if 'J' in v.refine_mode:
         for i in range(len(v.atomic_sites)):
-            
             v.refined_variable.append(v.Basis_Kappa[v.atomic_sites[i]])
             v.refined_variable_type.append(10)
             v.atom_refine_flag.append(v.atomic_sites[i])
             v.atom_refine_vec.append(nullvec)  # no atom movement
 
-       
-    if 'K' in v.refine_mode:  
+    if 'K' in v.refine_mode:
         for i in range(len(v.atomic_sites)):
-            
             v.refined_variable.append(v.Basis_Pv[v.atomic_sites[i]])
             v.refined_variable_type.append(11)
             v.atom_refine_flag.append(v.atomic_sites[i])
             v.atom_refine_vec.append(nullvec)  # no atom movement
-       
-        
-       
 
     # Total number of independent variables
     v.n_variables = len(v.refined_variable)

pylix_modules/pylix.py
@@ -1084,6 +1084,7 @@ def Fg_matrix(n_hkl, scatter_factor_method, n_atoms, atom_coordinate,
     # equivalent anisotropic B, size [n_atoms, n_hkl, n_hkl]
     B_aniso = np.divide(Ugg, np.square(g_magnitude), out=np.zeros_like(Ugg),
                         where=(g_magnitude != 0)) * 8 * np.pi**2
+    print(B_aniso[:, 0, 0])
 
     Fg_matrix = np.zeros([n_hkl, n_hkl], dtype=np.complex128)
     # scattering factor f_g, size [n_hkl, n_hkl], atom by atom
@@ -1099,8 +1100,8 @@ def Fg_matrix(n_hkl, scatter_factor_method, n_atoms, atom_coordinate,
             f_g = f_doyle_turner(atomic_number[i], g_magnitude)
         elif scatter_factor_method == 4:
             print("Calculating scattering factors for atom", i+1, "/", n_atoms)
-            f_g = kappa_factors(g_magnitude,atomic_number[i],pv[i],kappas[i])
-        else: 
+            f_g = kappa_factors(g_magnitude, atomic_number[i], pv[i], kappas[i])
+        else:
             raise ValueError("No scattering factors chosen in felix.inp")
 
         # get the absorptive scattering factor (null for absorption_method==0)

pylix_modules/simulate.py
@@ -91,7 +91,6 @@ def simulate(v):
     # mean inner potential as the sum of scattering factors at g=0
     # multiplied by h^2/(2pi*m0*e*CellVolume)
     mip = 0.0
-    print(n_atoms)
     for i in range(n_atoms):  # get the scattering factor
         if v.scatter_factor_method == 0:
             mip += px.f_kirkland(atomic_number[i], 0.0)
@@ -477,52 +476,35 @@ def update_variables(v):
                 v.basis_B_iso[v.atom_refine_flag[i]] = v.refined_variable[i]*1.0
             else:
                 v.basis_B_iso[v.atom_refine_flag[i]] = 0.0
-        
-        
-        
-        
         elif variable_type == 5:
-            
-            
             # Aniso Debye-Waller factor (implemented)
-            
-             
-             U = v.aniso_matrix[v.atom_refine_flag[i]]
-             if   0 < U[0,0] < 0.1:
-                 U[0,0]= v.refined_variable[i]*1.0
-             else:
-                 U[0,0]=0
-             if   0 < U[1,1] < 0.1:
-                  U[1,1]= v.refined_variable[i]*1.0
-             else:
-                  U[1,1]=0
-             if   0 < U[2,2] < 0.1:
-                  U[2,2]= v.refined_variable[i]*1.0
-             else:
-                  U[2,2]=0
-             if   0 < U[0,1] < 0.1:
-                  U[0,1]= U[1,0] = v.refined_variable[i]*1.0
-             else:
-                  U[0,1]= U[1,0]= 0
-             
-             
-             if   0 < U[0,2] < 0.1:
-                 U[0,2]= U[2,0]= v.refined_variable[i]*1.0
-             else:
-                 U[0,2]= U[2,0]= 0
-            
-             
-             if   0 < U[1,2] < 0.1:
-                  U[1,2]= U[1,2]= v.refined_variable[i]*1.0
-             else:
-                  U[1,2]= U[2,1]=0
-                  
-                 
-                 
-                
-             v.aniso_matrix[v.atom_refine_flag[i]] = U
+            U = v.aniso_matrix[v.atom_refine_flag[i]]
+            if 0 < U[0, 0] < 0.1:
+                U[0, 0] = v.refined_variable[i]*1.0
+            else:
+                U[0, 0] = 0
+            if 0 < U[1, 1] < 0.1:
+                U[1, 1] = v.refined_variable[i]*1.0
+            else:
+                U[1, 1] = 0
+            if 0 < U[2, 2] < 0.1:
+                U[2, 2] = v.refined_variable[i]*1.0
+            else:
+                U[2, 2] = 0
+            if 0 < U[0, 1] < 0.1:
+                U[0, 1] = U[1, 0] = v.refined_variable[i]*1.0
+            else:
+                U[0, 1] = U[1, 0] = 0
+            if 0 < U[0, 2] < 0.1:
+                U[0, 2] = U[2, 0] = v.refined_variable[i]*1.0
+            else:
+                U[0, 2] = U[2, 0] = 0
+            if 0 < U[1, 2] < 0.1:
+                U[1, 2] = U[1, 2] = v.refined_variable[i]*1.0
+            else:
+                U[1, 2] = U[2, 1]=0
+            v.aniso_matrix[v.atom_refine_flag[i]] = U
 
-       
         elif variable_type == 6:
             # Lattice parameters a, b, c
             if v.refined_variable_type[i] == 6:
@@ -557,26 +539,13 @@ def update_variables(v):
             else:
              #   v.Basis_Kappa[v.atom_refine_flag[i]] = 0.0
                 v.Basis_Kappa[v.atom_refine_flag[i]] = np.clip(v.refined_variable[i], 0.7, 1.3)
-
-
-            
-           
-            #refining Pv values in basis 
+        # refining Pv values in basis 
         elif variable_type == 11:
            if v.refined_variable[i]*0.5 < v.refined_variable[i] < v.refined_variable[i]*1.5:  # must lie in a reasonable range
                 v.Basis_Pv[v.atom_refine_flag[i]] = v.refined_variable[i]*1.0
            else: 
                 v.Basis_Pv[v.atom_refine_flag[i]] = 0.0
                #v.Basis_Pv[v.atom_refine_flag[i]] = np.clip(v.refined_variable[i], ,1.5 )
-            
-        
-            
-            
-            
-        
-            
-            
-
 
     return
 
@@ -585,10 +554,11 @@ def print_LACBED(v):
     '''
     Plots all LACBED patterns in a montage
     '''
-    n = v.lacbed_sim.shape[3]# actual size, if felix.hkl's missing
+    n = v.lacbed_sim.shape[3]  # actual size, if felix.hkl's missing
     w = int(np.ceil(np.sqrt(n)))
     h = int(np.ceil(n/w))
     # only print all thicknesses for the first simulation
+    print(v.iter_count)
     if v.iter_count == 1:
         for j in range(v.n_thickness):
             fig, axes = plt.subplots(w, h, figsize=(w*5, h*5))